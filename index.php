ef84e48e779a090357471213dc4bb257f7da4b83
Luis Alejandro Avila Fajardo <alejandroa@MacBook-Air-de-Luis.local>
2022-10-25 20:57:06 -0500

    Simulated database with JSON file

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
-<?php 
-$contacts = [
-  ["name" => "pepe", "phone_number" => "32131313"],
-  ["name" => "antonio", "phone_number" => "321343255"],
-  ["name" => "nate", "phone_number" => "3432414232"]
-];
+<?php
+if (file_exists("contacts.json")) {
+  $contacts = json_decode(file_get_contents("contacts.json"), true);
+} else {
+  $contacts = [];
+}
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,11 +12,25 @@ $contacts = [
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <!-- Bootstrap -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.2.2/darkly/bootstrap.min.css" integrity="sha512-8RiGzgobZQmqqqJYja5KJzl9RHkThtwqP1wkqvcbbbHNeMXJjTaBOR+6OeuoxHhuDN5h/VlgVEjD7mJu6KNQXA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-  <!-- static content -->
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.3/darkly/bootstrap.min.css"
+    integrity="sha512-ZdxIsDOtKj2Xmr/av3D/uo1g15yxNFjkhrcfLooZV5fW0TT7aF7Z3wY1LOA16h0VgFLwteg14lWqlYUQK3to/w=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  />
+  <script
+    defer
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"
+  ></script>
+
+  <!-- Static Content -->
   <link rel="stylesheet" href="./static/css/index.css" />
+
   <title>Contacts App</title>
 </head>
 <body>
@@ -44,16 +57,26 @@ $contacts = [
             <a class="nav-link" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="./add.html">Add Contact</a>
+            <a class="nav-link" href="add.php">Add Contact</a>
           </li>
         </ul>
       </div>
     </div>
   </nav>
+
   <main>
-    <div class="container pt-4 pt-3">
+    <div class="container pt-4 p-3">
       <div class="row">
-        <?php foreach ($contacts as $contact): ?> 
+        
+        <?php if (count($contacts) == 0): ?>
+          <div class="col-md-4 mx-auto">
+            <div class="card card-body text-center">
+              <p>No contacts saved yet</p>
+              <a href="add.php">Add One!</a>
+            </div>
+          </div>
+        <?php endif ?>
+        <?php foreach ($contacts as $contact): ?>
           <div class="col-md-4 mb-3">
             <div class="card text-center">
               <div class="card-body">
@@ -65,6 +88,7 @@ $contacts = [
             </div>
           </div>
         <?php endforeach ?>
+
       </div>
     </div>
   </main>
